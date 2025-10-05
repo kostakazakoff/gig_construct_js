2571ff0831c2c4d9a0f4f837f2e9e18a7b97c9a4
Kosta Kazakov <kostakazakoff@gmail.com>
2025-10-05 17:30:42 +0300

    projects page - 2 cols

--- src/app/projects/client.jsx
+++ src/app/projects/client.jsx
@@ -24,7 +24,7 @@ export default function ProjectsComponent() {
 
     return (
         <div className='relative my-4'>
-            <section className='flex flex-col gap-4'>
+            <section className='grid grid-cols-1 xl:grid-cols-2 gap-16 px-8 xl:px-24 2xl:px-40'>
                 {translated.map((details) => (
                     <ProjectCard key={details.id} project={details} staticData={translatedStaticData} />
                 ))}
