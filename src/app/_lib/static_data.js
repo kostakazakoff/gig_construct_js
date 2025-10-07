ace34372d582956105ef62928f42b2eb6af61199
PREDATOR\kosta <kostakazakoff@gmail.com>
2025-10-07 14:19:23 +0300

    nothing

--- src/app/_lib/static_data.js
+++ src/app/_lib/static_data.js
@@ -3,31 +3,32 @@ export const servicesStaticData = {
         legend: "Всички цени са с включен ДДС. Цените са ориентировъчни и могат да варират в зависимост от спецификата на обекта и изискванията на клиента. За точна оферта, моля свържете се с нас.",
         currency: "лв",
         askOfferText: "Поискай оферта",
-        estimatedPrice: "Ориентировъчна цена за труд"
+        estimatedPrice: "Ориентировъчна цена за труд",
     },
     EN: {
         legend: "All prices include VAT. Prices are indicative and may vary depending on the specifics of the site and customer requirements. For an accurate quote, please contact us.",
         currency: "BGN",
         askOfferText: "Ask for an offer",
-        estimatedPrice: "Estimated labor costs"
-    }
+        estimatedPrice: "Estimated labor costs",
+    },
 };
 
 export const projectsStaticData = {
     BG: {
         value: "Приблизителна стойност",
-        currency: "лв"
+        currency: "лв",
     },
     EN: {
         value: "Аpproximate value",
-        currency: "BGN"
-    }
+        currency: "BGN",
+    },
 };
 
 export const aboutStaticData = {
     BG: {
         title: "За нас",
-        aboutText: "Добре дошли в GiG Construct!\n\n" +
+        aboutText:
+            "Добре дошли в GiG Construct!\n\n" +
             "Ние сме строителна компания, специализирана в предоставянето на висококачествени ремонтни услуги, за жилищни или търговски сгради.\n\n" +
             "Екип от опитни специалисти и ангажимент към превъзходство, ние се гордеем с това, че превръщаме визиите на нашите клиенти в реалност.\n\n" +
             "Нашата мисия е да доставяме изключителни строителни решения, независимо дали става въпрос за ново строителство, ремонт или специализирани услуги, ние подхождаме към всеки проект с внимание към детайла, иновации и ангажираност към качеството.",
@@ -35,13 +36,14 @@ export const aboutStaticData = {
     },
     EN: {
         title: "About Us",
-        aboutText: "Welcome to GiG Construct!\n\n" +
+        aboutText:
+            "Welcome to GiG Construct!\n\n" +
             "We are a construction company specializing in providing high-quality renovation services for residential or commercial buildings.\n\n" +
             "With a team of experienced professionals and a commitment to excellence, we take pride in turning our clients' visions into reality.\n\n" +
             "Our mission is to deliver exceptional construction solutions, whether it's new construction, renovation, or specialized services, we approach each project with attention to detail, innovation, and a dedication to quality.",
         partnersTitle: "Our Partners and Clients",
-    }
-}
+    },
+};
 
 export const buttonsStaticData = {
     BG: {
@@ -53,28 +55,28 @@ export const buttonsStaticData = {
         submit: "Submit",
         cancel: "Cancel",
         close: "Close",
-    }
-}
+    },
+};
 
 export const navTranslations = {
     BG: {
-        home: 'Начало',
-        about: 'За нас',
-        services: 'Услуги',
-        projects: 'Проекти',
-        news: 'Новини',
-        contact: 'Контакти',
-        clients_partners: 'Клиенти и партньори',
+        home: "Начало",
+        about: "За нас",
+        services: "Услуги",
+        projects: "Проекти",
+        news: "Новини",
+        contact: "Контакти",
+        clients_partners: "Клиенти и партньори",
     },
     EN: {
-        home: 'Home',
-        about: 'About Us',
-        services: 'Services',
-        projects: 'Projects',
-        news: 'News',
-        contact: 'Contact',
-        clients_partners: 'Clients and Partners',
-    }
+        home: "Home",
+        about: "About Us",
+        services: "Services",
+        projects: "Projects",
+        news: "News",
+        contact: "Contact",
+        clients_partners: "Clients and Partners",
+    },
 };
 
 export const footerStaticData = {
@@ -87,7 +89,7 @@ export const footerStaticData = {
         contactUs: "Contact Us",
         followUs: "Follow Us",
         rightsReserved: "All rights reserved.",
-    }
+    },
 };
 
 export const offerNoteStaticData = {
@@ -116,5 +118,5 @@ export const offerNoteStaticData = {
         successMessage: "Your message was sent successfully!",
         errorMessage: "An error occurred while sending your message. Please try again later.",
         title: "Request a Free Offer",
-    }
-}
+    },
+};
