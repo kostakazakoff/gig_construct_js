94c934288e7ad0699eec26fc6e208a28e02933c1
PREDATOR\kosta <kostakazakoff@gmail.com>
2025-10-07 13:30:50 +0300

    App font changed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { Raleway } from "next/font/google";
+import { Comfortaa } from "next/font/google";
 import "./globals.css";
 import "@tailwindplus/elements";
 import Navbar from "@/app/_components/mainComponents/navbar.jsx";
@@ -6,46 +6,42 @@ import Footer from "@/app/_components/mainComponents/footer.jsx";
 import { LanguageProvider } from "./_hooks/useLanguageContext.jsx";
 import { ThemeProvider } from "./_hooks/useTheme.jsx";
 
-const raleway = Raleway({
-  variable: "--font-raleway",
-  subsets: ["latin"],
-  display: "swap",
+const comfortaa = Comfortaa({
+    variable: "--font-comfortaa",
+    subsets: ["latin"],
+    display: "swap",
 });
 
 export const metadata = {
-  title: {
-    default: "GIG Construct",
-    template: "%s | GIG Construct",
-  },
-  icons: {
-    icon: "/gig_logo_white.svg",
-  },
-  description:
-    "Строителнини услуги, ремонтни дейности, електро услуги, ВиК услуги, гипсокартон, боядисване, изолации, шпакловка, плочки, гранитогрес, извозване на строителни отпадъци.",
+    title: {
+        default: "GIG Construct",
+        template: "%s | GIG Construct",
+    },
+    icons: {
+        icon: "/gig_logo_white.svg",
+    },
+    description:
+        "Строителнини услуги, ремонтни дейности, електро услуги, ВиК услуги, гипсокартон, боядисване, изолации, шпакловка, плочки, гранитогрес, извозване на строителни отпадъци.",
 };
 
-export default function RootLayout({
-  children,
-}) {
-  return (
-    <html lang="en">
-      <body
-        className={`${raleway.variable} antialiased font-sans`}
-      >
-        <ThemeProvider>
-          <LanguageProvider>
-            <header>
-              <Navbar />
-            </header>
-            <main className="min-h-screen bg-slate-200 dark:bg-slate-900 text-slate-900 dark:text-slate-200 text-sm sm:text-base flex items-start justify-center py-22">
-              {children}
-            </main>
-            <footer>
-              <Footer />
-            </footer>
-          </LanguageProvider>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className={`${comfortaa.variable} antialiased font-sans`}>
+                <ThemeProvider>
+                    <LanguageProvider>
+                        <header>
+                            <Navbar />
+                        </header>
+                        <main className="min-h-screen bg-slate-200 dark:bg-slate-900 text-slate-900 dark:text-slate-200 text-sm sm:text-base flex items-start justify-center py-22">
+                            {children}
+                        </main>
+                        <footer>
+                            <Footer />
+                        </footer>
+                    </LanguageProvider>
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
