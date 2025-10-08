702e400434173f78e894aa71e8b9ee9d4bb3e14d
Kosta Kazakov <kostakazakoff@gmail.com>
2025-10-08 09:15:19 +0300

    css - projects_card

--- src/app/_components/projects_card.jsx
+++ src/app/_components/projects_card.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function ProjectsCard({ project, staticData }) {
+export default function ProjectsCard({ project, staticData, language }) {
     return (
         <div className="group max-w-xl hover:shadow-xl hover:scale-105 transition duration-300 ease-in-out border-l-6 border-b-1 border-r-1 hover:border-gig-blue dark:hover:border-blue-400 rounded-lg">
             <div className="px-6 py-4 text-slate-700 font-bold font-medium rounded-t-sm bg-slate-300 dark:bg-slate-800 dark:text-slate-200 flex justify-between max-md:flex-col max-md:gap-2 max-md:items-center group-hover:bg-gig-blue dark:group-hover:bg-blue-400 group-hover:text-white transition duration-300 ease-in-out shadow-md">
@@ -21,17 +21,19 @@ export default function ProjectsCard({ project, staticData }) {
                         </p>
                         <hr className="max-md:hidden" />
                         <div className="mt-4">
-                            <div className="relative overflow-hidden h-64 w-80 rounded-sm bg-slate-300 shrink-0 shadow-md border-8 border-slate-100 dark:border-slate-700 group-hover:opacity-75 group-hover:grayscale-80 transition duration-400 ease-in-out:grayscale float-left max-xl:mb-4 md:mr-4">
+                            <div className="relative overflow-hidden h-48 w-64 rounded-sm bg-slate-300 shrink-0 shadow-md border-8 border-slate-100 dark:border-slate-700 group-hover:opacity-75 group-hover:grayscale-80 transition duration-400 ease-in-out:grayscale float-left max-xl:mb-4 md:mr-4">
                                 <Image
-                                    width={64}
+                                    width={48}
                                     height={64}
                                     alt=""
                                     src={project.imageSrc ? project.imageSrc : "/photo.svg"}
                                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                    className="size-80 flex-none bg-slate-300 dark:bg-gray-800 dark:outline-white/10 object-cover"
+                                    className="size-64 flex-none bg-slate-300 dark:bg-gray-800 dark:outline-white/10 object-cover"
                                 />
                             </div>
-                            <p className="mt-1 text-sm text-justify indent-8 md:text-base">{project.description}</p>
+                            <p className="mt-1 text-sm text-justify indent-8 md:text-base hyphens-auto" lang={language.toLowerCase()}>
+                                {project.description}
+                            </p>
                         </div>
                     </div>
                 </div>
--- src/app/projects/client.jsx
+++ src/app/projects/client.jsx
@@ -26,7 +26,7 @@ export default function ProjectsComponent() {
         <div className='relative my-4'>
             <section className='grid grid-cols-1 xl:grid-cols-2 gap-16 px-8 xl:px-24 2xl:px-40'>
                 {translated.map((details) => (
-                    <ProjectCard key={details.id} project={details} staticData={translatedStaticData} />
+                    <ProjectCard key={details.id} project={details} staticData={translatedStaticData} language={language} />
                 ))}
             </section>
         </div>
