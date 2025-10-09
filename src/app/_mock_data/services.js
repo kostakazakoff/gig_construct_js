cf90cd921da3705d39a456241169ab3de7d479c1
Kosta Kazakov <kostakazakoff@gmail.com>
2025-10-09 20:55:59 +0300

    mock data change

--- src/app/_mock_data/services.js
+++ src/app/_mock_data/services.js
@@ -5,84 +5,84 @@ export const servicesData = {
             name: "Боядисване",
             description: "Подготовка, за грундиране и боядисване на различни повърхности.",
             src: "/images/services/paint.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         2: {
             id: 2,
             name: "ВиК услуги",
             description: "Проектиране, Монтаж и Демонтаж на Водопреносна инсталация.",
             src: "/images/services/plumbing.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на контактна точка и линеен метър."
         },
         3: {
             id: 3,
             name: "Гипсокартон",
             description: "Монтаж, Демонтаж и Подмяна на Гипсокартон, Изграждане на стени, Обшивки, Окачени тавани.",
             src: "/images/services/drywall.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         4: {
             id: 4,
             name: "Електро услуги",
             description: "Проектиране, Монтаж и Демонтаж на електроинсталации.",
             src: "/images/services/electrical.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на контактна точка и линеен метър."
         },
         5: {
             id: 5,
             name: "Замазки",
             description: "Машинно и ръчно полагане на различен по вид замаски, изправяне на стени.",
             src: "/images/services/screeds.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         6: {
             id: 6,
             name: "Зидария",
             description: "Зидане с тухла, бетонни блокчета, газобетон Ytong.",
             src: "/images/services/masonry.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         7: {
             id: 7,
             name: "Настилки",
             description: "Полагане на Подови или Бетонни настилки.",
             src: "/images/services/flooring.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         8: {
             id: 8,
             name: "Окачени тавани",
             description: "Монтаж на окачени тавани от различен вид материали.",
             src: "/images/services/celling.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         9: {
             id: 9,
             name: "Плочки, гранитогрес",
             description: "Полагане на Плочки фаянс и теракот плочки - Гранитогрес, Естествен или изкуствен камък.",
             src: "/images/services/tiles.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         10: {
             id: 10,
             name: "Изолация",
             description: "Полагане на Топло и Хидро изолация.",
             src: "/images/services/insulation.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         11: {
             id: 11,
             name: "Шпакловка",
             description: "Гипсова, Циментова, шпакловка армиране с мрежа,  шпакловка или гласфазерен воал.",
             src: "/images/services/putty.jpeg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на квадратен метър."
         },
         12: {
             id: 12,
             name: "Почистване",
             description: "Почистване и извозване на строителни отпадъци.",
             src: "/images/services/cleaning.jpg",
-            note: "The pricing of the service is calculated per contact point and linear meter."
+            note: "Ценообразуването на услугата се изчислява на килограм."
         },
     },
     EN: {
