ae686e4a9ebe7e95750e9b8c4c04b8697dd39310
PREDATOR\kosta <kostakazakoff@gmail.com>
2025-11-07 00:35:30 +0200

    working on newsCard, news page

---
+++ src/app/_components/newsComponents/newsCard.jsx
@@ -0,0 +1,36 @@
+import { API_PATH } from "@/app/_lib/api_paths";
+import Image from "next/image";
+
+export default function NewsCard({ newsItem }) {
+
+    const addImage = (imageSrc) => {
+        if (!imageSrc) {
+            return "/gig_logo.svg";
+        } else if (imageSrc.startsWith('http')) {
+            return imageSrc;
+        }
+        return API_PATH.BACKEND_URL + imageSrc;
+    }
+
+    return (
+        <div className="max-w-6xl">
+            <h2 className="text-3xl lg:text-4xl font-bold mb-6 text-slate-800 leading-none border-b-2 pb-2">
+                {newsItem.title}
+            </h2>
+            <div className="relative sm:mb-1 overflow-hidden h-48 w-64 rounded-sm shrink-0 shadow-md border-8 border-slate-100 dark:border-slate-700 float-left mr-4">
+                <Image
+                    src={addImage(newsItem.image_src)}
+                    alt={newsItem.title}
+                    width={96}
+                    height={48}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    className="size-96 object-cover self-center"
+                />
+            </div>
+            <p className="mt-1 text-sm text-justify indent-8 md:text-base hyphens-auto">{newsItem.content}
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum corporis expedita at architecto consectetur. Porro, laborum? Adipisci incidunt ipsa aut iusto ullam, blanditiis maiores? Alias consequatur, doloribus, corporis porro vitae modi in natus eaque hic eveniet reiciendis magni eligendi optio, soluta illo illum aspernatur maiores atque similique nisi mollitia voluptatem. Incidunt laborum eaque dolore explicabo cum, odio sit dicta rerum voluptate totam unde debitis sint officia nihil at magnam vero beatae aliquid laboriosam? Unde, quos soluta provident iste, enim fugit ut at quia doloribus veritatis facilis fuga? Repudiandae, sapiente nesciunt voluptatum sunt aspernatur laborum velit similique sint dolore ab necessitatibus neque atque quasi eveniet alias ut exercitationem libero! Sequi commodi asperiores illo, perspiciatis unde qui deserunt accusantium tenetur optio nemo repellendus! Earum asperiores iusto praesentium beatae odio ad veritatis soluta, quod fugiat voluptas hic reprehenderit dignissimos alias nisi corporis sit molestiae aliquid repudiandae, omnis et. Excepturi, minima, pariatur labore et fuga possimus natus debitis, placeat omnis reprehenderit aut officiis sunt! Soluta aperiam ut perspiciatis voluptates inventore odio tenetur praesentium ad natus! Voluptates voluptas, harum cupiditate quod, nisi ad modi natus iure ab magni ex mollitia laborum, sint eligendi numquam? Optio vitae corporis nisi error quasi tempore laudantium voluptate, dolorum consequatur.
+            </p>
+        </div>
+    );
+}
+
--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,4 +1,6 @@
 import CompLoader from "../_components/mainComponents/compLoader";
+import NewsCard from "../_components/newsComponents/newsCard";
+import be from "../_utils/Api";
 
 export const metadata = {
     title: 'News',
@@ -6,10 +8,31 @@ export const metadata = {
     tags: ['news', 'updates', 'construction', 'renovation'],
 }
 
-export default function NewsPage() {
+export default async function NewsPage() {
+    let news = null;
+    
+    try {
+        const newsResponse = await be.get('news/');
+        if (newsResponse && newsResponse.data && newsResponse.data.succeed) {
+            news = newsResponse.data.data;
+        } else {
+            console.log('Failed to fetch news:', newsResponse?.message);
+        }
+    } catch (error) {
+        console.error('Error fetching news:', error);
+    }
+
     return (
-        <div>
-            <CompLoader />
-        </div>
+        news ? (
+            <div className="flex flex-col gap-8 px-8 py-8">
+                {news.map((item) => (
+                    <NewsCard key={item.id} newsItem={item} />
+                ))}
+            </div>
+        ) : (
+            <div>
+                <CompLoader />
+            </div>
+        )
     );
 }
