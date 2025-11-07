90aeef405db6a06e1a03d8ea403a7be677cc8412
PREDATOR\kosta <kostakazakoff@gmail.com>
2025-11-08 00:35:41 +0200

    css

--- src/app/_components/newsComponents/newsCard.jsx
+++ src/app/_components/newsComponents/newsCard.jsx
@@ -22,12 +22,12 @@ export default function NewsCard({ newsItem }) {
     }
 
     return (
-        <div className="max-w-6xl pl-8 py-2 border-l-4 border-slate-800 dark:border-slate-200">
+        <div className="max-w-6xl pl-8 py-4 border-l-4 border-gig-blue">
             <div className="flex justify-between items-start gap-24 mb-6">
-                <h2 className="text-3xl lg:text-4xl font-bold text-slate-800 dark:text-slate-200 leading-none pb-2">
+                <h2 className="text-3xl lg:text-4xl font-bold text-slate-800 dark:text-slate-200 px-8 py-4 bg-slate-100 dark:bg-slate-700 border-b-2 border-slate-300 dark:border-slate-700">
                     {newsItem.title}
                 </h2>
-                <p className="text-sm text-slate-600 dark:text-slate-400 whitespace-nowrap">
+                <p className="text-slate-600 dark:text-slate-400 whitespace-nowrap underline decoration-dotted">
                     {formatDate(newsItem.created_at)}
                 </p>
             </div>
