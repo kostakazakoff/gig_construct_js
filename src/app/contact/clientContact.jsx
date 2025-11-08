aa50c3323b4e995a549e4bfa457ede6fcbbfce37
PREDATOR\kosta <kostakazakoff@gmail.com>
2025-11-08 19:23:38 +0200

    css

--- src/app/contact/clientContact.jsx
+++ src/app/contact/clientContact.jsx
@@ -24,19 +24,7 @@ export default function ClientContact() {
                     <p className="mt-2">{contactData.companyEmail}</p>
             </section>
             
-            <section className="isolate bg-slate-50 dark:bg-slate-700 px-6 py-24 sm:py-32 lg:px-8 max-w-xl rounded-b-lg">
-                <div
-                    aria-hidden="true"
-                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-                >
-                    <div
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                        className="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-288.75"
-                    />
-                </div>
+            <section className="bg-slate-50 dark:bg-slate-700 px-6 py-24 sm:py-32 lg:px-8 max-w-xl rounded-b-lg">
                 <div className="mx-auto max-w-2xl text-center">
                     <h2 className="text-4xl font-semibold tracking-tight text-balance sm:text-5xl">{contactData.title}</h2>
                     <p className="mt-2 ">{contactData.description}</p>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -33,7 +33,20 @@ export default function RootLayout({ children }) {
                         <header>
                             <Navbar />
                         </header>
-                        <main className="min-h-screen bg-slate-200 dark:bg-slate-900 text-slate-800 dark:text-slate-200 text-sm sm:text-base flex items-start justify-center py-16 lg:py-20">
+                        <main className="isolate min-h-screen bg-slate-200 dark:bg-slate-900 text-slate-800 dark:text-slate-200 text-sm sm:text-base flex items-start justify-center py-16 lg:py-20">
+
+                            <div
+                                aria-hidden="true"
+                                className="fixed inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                            >
+                                <div
+                                    style={{
+                                        clipPath:
+                                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                                    }}
+                                    className="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-288.75"
+                                />
+                            </div>
                             {children}
                         </main>
                         <footer>
