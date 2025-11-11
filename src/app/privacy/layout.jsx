bbb401ebbc9296f44b0e546b4963e609d0b96581
PREDATOR\kosta <kostakazakoff@gmail.com>
2025-11-11 18:00:17 +0200

    change privacy/layout.jsx behavior

--- src/app/privacy/layout.jsx
+++ src/app/privacy/layout.jsx
@@ -1,21 +1,21 @@
 'use client';
 
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import useLanguageContext from '@/app/_hooks/useLanguageContext';
 
 export default function ProjectLayout({ children }) {
     const { language } = useLanguageContext();
+    const router = useRouter();
 
     return (
         <section>
             <div className="w-full bg-slate-900 dark:bg-slate-800 text-slate-200 py-4 fixed left-0 z-50 text-center">
-                    <Link
-                        href={`contact`}
+                    <button
+                        onClick={() => router.back()}
                         className="rounded-full bg-slate-900 dark:bg-slate-800 px-3.5 py-1 text-sm font-semibold text-white shadow-xs hover:bg-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
-                        scroll={true}
                     >
-                        <span aria-hidden="true">&larr;</span> {language === 'EN' ? 'Back to contacts page' : 'Назад към контакти'}
-                    </Link>
+                        <span aria-hidden="true">&larr;</span> {language === 'EN' ? 'Back' : 'Назад'}
+                    </button>
             </div>
 
             <div className="py-24">
