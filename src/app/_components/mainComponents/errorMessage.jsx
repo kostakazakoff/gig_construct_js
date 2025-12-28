1dececce47a1b2cd6bbb72b8ec3da6a1711d8886
Kosta <kostakazakoff@gmail.com>
2025-12-28 17:46:25 +0200

    clean

--- src/app/_components/mainComponents/errorMessage.jsx
+++ src/app/_components/mainComponents/errorMessage.jsx
@@ -6,9 +6,9 @@ export default function ErrorMessage({ formError, closeWrapper }) {
 
     return (
         <div
-        className={`transition-opacity duration-300 ease-in-out bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative cursor-pointer ${formError ? 'opacity-100 block' : 'opacity-0 hidden'}`}
-        role="alert"
-        onClick={closeWrapper}
+            className={`transition-opacity duration-300 ease-in-out bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative cursor-pointer ${formError ? 'opacity-100 block' : 'opacity-0 hidden'}`}
+            role="alert"
+            onClick={closeWrapper}
         >
             <strong className="font-bold"></strong>{formError}
         </div>
