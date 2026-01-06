21367baf9e04455bd113c61d98c907649d38ab10
Kosta <kostakazakoff@gmail.com>
2026-01-06 09:07:17 +0200

    refactor: comment out authentication options in API configuration

--- src/app/_utils/Api.js
+++ src/app/_utils/Api.js
@@ -3,8 +3,11 @@ import { API_PATH } from "@/app/_lib/api_paths.js";
 
 const be = axios.create({
     baseURL: `http://${API_PATH.ORIGIN}/`,
-    withCredentials: true,
-    withXSRFToken: true,
+
+    // No authentication for now
+    // withCredentials: true,
+    // withXSRFToken: true,
+
     headers: {
         Accept: "application/json",
     },
