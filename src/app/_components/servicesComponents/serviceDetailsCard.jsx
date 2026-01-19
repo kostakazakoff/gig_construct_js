543ea405eb9bb0088cf9a5b2cb8461e97dd1fd53
PREDATOR\kosta <kostakazakoff@gmail.com>
2026-01-19 22:25:25 +0200

    refactor: update ServiceDetailsCard to use API_PATH for service link formatting

--- src/app/_components/servicesComponents/serviceDetailsCard.jsx
+++ src/app/_components/servicesComponents/serviceDetailsCard.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { DivideIcon } from "@heroicons/react/24/solid";
+import { API_PATH } from "@/app/_lib/api_paths";
 
 export default function ServiceDetailsCard({ serviceId, service, servicesStaticData }) {
     console.log("service data:", service);
@@ -10,7 +11,7 @@ export default function ServiceDetailsCard({ serviceId, service, servicesStaticD
     }
 
     return (
-        <Link key={serviceId} href={`/services/${serviceId}?modal=true`} passHref>
+        <Link key={serviceId} href={`${API_PATH.SERVICES}/${serviceId}?modal=true`} passHref>
             <article className="group w-96 h-64 grid grid-rows-4 transition duration-300 ease-in-outhover:translate-y-1 hover:scale-105 shadow-md/30 hover:shadow-xl/40 rounded-sm relative overflow-hidden bg-gig-blue/20 dark:bg-slate-800/90 text-slate-200">
                 <div className="relative row-span-3 overflow-hidden object-cover align-middle w-full h-full">
                     <div>
