915745e50e005df8e872fb0ca874d5d211b63b7e
PREDATOR\kosta <kostakazakoff@gmail.com>
2026-01-20 14:53:56 +0200

    refactor: remove commented-out redirect logic from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Redirect from root to /services
-    // async redirects() {
-    //     return [
-    //         {
-    //             source: "/",
-    //             destination: "/services",
-    //             permanent: true,
-    //         },
-    //     ];
-    // },
-
     images: {
         remotePatterns: [
             {
