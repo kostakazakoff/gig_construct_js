a58da6009a17030585b00097d7f778bc34f808e0
Kosta <kostakazakoff@gmail.com>
2026-01-27 22:34:53 +0200

    refactor: update ProjectsCard component to manage navigation direction based on media availability

--- src/app/_components/projectsComponents/projectsCard.jsx
+++ src/app/_components/projectsComponents/projectsCard.jsx
@@ -10,6 +10,8 @@ export default function ProjectsCard({ project, staticData, language }) {
 
     const [img, setImg] = useState("/GIG_logo_white.png");
 
+    const [direction, setDirection] = useState("#");
+
     useEffect(() => {
         const resultUrl = resolveImageUrl(project.image_thumb_src);
         if (!resultUrl.endsWith("null")) {
@@ -19,25 +21,25 @@ export default function ProjectsCard({ project, staticData, language }) {
         }
     }, [project.image_thumb_src]);
 
+    useEffect(() => {
+        if (project.media.length > 0) {
+            setDirection(`/projects/${project.id}`);
+        } else {
+            setDirection("#");
+        }
+    }, [project.media]);
+
     return (
         <article
             className="group max-w-xl shadow-md/20 hover:shadow-xl/20 hover:scale-105 transition duration-300 ease-in-out border-l-6 border-b-1 border-r-1 hover:border-gig-blue dark:hover:border-blue-400 rounded-lg"
-            id={project.id}
-        >
-            <Link
-                href={`/projects/${project.id}`}
-                passHref
-            >
+            id={project.id}>
+
+            <Link href={direction} passHref>
                 <div className="px-6 py-4 text-slate-700 font-bold font-medium rounded-t-sm bg-slate-300 dark:bg-slate-800 dark:text-slate-200 flex justify-between max-md:flex-col max-md:gap-2 max-md:items-center group-hover:bg-gig-blue dark:group-hover:bg-blue-400 group-hover:text-white transition duration-300 ease-in-out shadow-md">
                     <p>{formatDate(project.date)}</p>
-                    <p>
-                        {staticData.value} {project.price} {staticData.currency}
-                    </p>
+                    <p>{staticData.value} {project.price} {staticData.currency}</p>
                 </div>
-                <div
-                    className="flex flex-col items-strech md:flex-row md:justify-items-strech md:items-strech gap-8 p-4 md:p-8 transition duration-300 ease-in-outhover:translate-y-1 mt-4"
-
-                >
+                <div className="flex flex-col items-strech md:flex-row md:justify-items-strech md:items-strech gap-8 p-4 md:p-8 transition duration-300 ease-in-outhover:translate-y-1 mt-4">
                     <div className="min-w-0 w-full md:w-140 xl:w-160 2xl:w-200 h-auto flex flex-col justify-between">
                         <div className="min-w-0 mb-auto w-full">
                             <p className="hyphens-auto text-xl md:text-3xl lg:text-4xl font-bold uppercase mb-6 text-slate-800 leading-none group-hover:text-gig-blue dark:group-hover:text-blue-400 dark:text-slate-300 break-words">
